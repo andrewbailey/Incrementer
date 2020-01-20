d4e17346f4b44ca946e483c7e1c757bb74c63cf7
Andrew Bailey <me@andrewbailey.dev>
2020-01-19 21:30:25 -0500

    Create launcher activity with implementations list

---
+++ app/src/main/java/dev/andrewbailey/incrementer/MainActivity.kt
@@ -0,0 +1,17 @@
+package dev.andrewbailey.incrementer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.andrewbailey.incrementer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+    }
+}
